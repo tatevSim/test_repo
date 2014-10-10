ccb73fb9cb5d36cae22434ed3a4678738bf24776
tatevSim <tatev-si@mail.ru>
2014-10-10 18:13:00 +0500

    add status

--- ImgLayout/src/com/example/imglayout/Planning.java
+++ ImgLayout/src/com/example/imglayout/Planning.java
@@ -1,7 +1,5 @@
 package com.example.imglayout;
 
-
-
 import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -34,244 +32,245 @@ import android.widget.ToggleButton;
 
 public class Planning extends ActionBarActivity {
 
-int count = 0;
+	int count = 0;
 
+	Calendar c = Calendar.getInstance();
+	int phour = c.get(Calendar.HOUR_OF_DAY);
+	int pminute = c.get(Calendar.MINUTE);
+	String time = phour + "" + ":" + pminute + "";
+	String days;
+	TextView countofdays;
+	private SharedPreferences sharedPreferences;
+	private boolean wasEdited = false;
 
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.planning);
+		countofdays = (TextView) findViewById(R.id.daycount);
 
-Calendar c = Calendar.getInstance(); 
-int phour = c.get(Calendar.HOUR_OF_DAY);
-int pminute = c.get(Calendar.MINUTE);
- TextView countofdays;
- private SharedPreferences sharedPreferences;
- private boolean wasEdited = false;
+		ViewHolder.mon = (ToggleButton) findViewById(R.id.toggMon);
+		ViewHolder.tue = (ToggleButton) findViewById(R.id.toggTue);
+		ViewHolder.wed = (ToggleButton) findViewById(R.id.toggWed);
+		ViewHolder.thu = (ToggleButton) findViewById(R.id.toggThu);
+		ViewHolder.fri = (ToggleButton) findViewById(R.id.toggFri);
+		ViewHolder.sat = (ToggleButton) findViewById(R.id.toggSat);
+		ViewHolder.sun = (ToggleButton) findViewById(R.id.toggSun);
+		ViewHolder.mon.setOnCheckedChangeListener(toggleButtonListener);
+		ViewHolder.tue.setOnCheckedChangeListener(toggleButtonListener);
+		ViewHolder.wed.setOnCheckedChangeListener(toggleButtonListener);
+		ViewHolder.thu.setOnCheckedChangeListener(toggleButtonListener);
+		ViewHolder.fri.setOnCheckedChangeListener(toggleButtonListener);
+		ViewHolder.sat.setOnCheckedChangeListener(toggleButtonListener);
+		ViewHolder.sun.setOnCheckedChangeListener(toggleButtonListener);
 
- @Override
- protected void onCreate(Bundle savedInstanceState) {
-  super.onCreate(savedInstanceState);
-  setContentView(R.layout.planning);
-  countofdays = (TextView) findViewById(R.id.daycount);
+		ViewHolder.datePickerButton = (Button) findViewById(R.id.datePickerButton);
+		ViewHolder.datePickerButton.setOnClickListener(new OnClickListener() {
 
+			@Override
+			public void onClick(View v) {
 
-  
+				Calendar mcurrentTime = Calendar.getInstance();
+				int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+				int minute = mcurrentTime.get(Calendar.MINUTE);
+				TimePickerDialog mTimePicker;
 
- ViewHolder.mon = (ToggleButton) findViewById(R.id.toggMon);
- ViewHolder.tue = (ToggleButton) findViewById(R.id.toggTue);
- ViewHolder.wed = (ToggleButton) findViewById(R.id.toggWed);
- ViewHolder.thu = (ToggleButton) findViewById(R.id.toggThu);
- ViewHolder.fri = (ToggleButton) findViewById(R.id.toggFri);
- ViewHolder.sat = (ToggleButton) findViewById(R.id.toggSat);
- ViewHolder.sun = (ToggleButton) findViewById(R.id.toggSun);
- ViewHolder.mon.setOnCheckedChangeListener(toggleButtonListener);
- ViewHolder.tue.setOnCheckedChangeListener(toggleButtonListener);
- ViewHolder.wed.setOnCheckedChangeListener(toggleButtonListener);
- ViewHolder.thu.setOnCheckedChangeListener(toggleButtonListener);
- ViewHolder.fri.setOnCheckedChangeListener(toggleButtonListener);
- ViewHolder.sat.setOnCheckedChangeListener(toggleButtonListener);
- ViewHolder.sun.setOnCheckedChangeListener(toggleButtonListener);
+				mTimePicker = new TimePickerDialog(Planning.this,
+						new TimePickerDialog.OnTimeSetListener() {
+							@Override
+							public void onTimeSet(TimePicker timePicker,
+									int selectedHour, int selectedMinute) {
+								ViewHolder.datePickerButton
+										.setText(selectedHour + ":"
+												+ selectedMinute);
+							}
+						}, hour, minute, true);
+				mTimePicker.setTitle("Select Time");
+				mTimePicker.show();
+				updateDisplay();
 
-  ViewHolder. datePickerButton = (Button) findViewById(R.id.datePickerButton);
-  ViewHolder.datePickerButton.setOnClickListener(new OnClickListener() {
+				ViewHolder.time = hour + ":" + minute + "";
+				Log.d("time", String.valueOf((ViewHolder.time)));
 
-   @Override
-   public void onClick(View v) {
+			}
 
-    Calendar mcurrentTime = Calendar.getInstance();
-   int   hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-    int minute = mcurrentTime.get(Calendar.MINUTE);
-    TimePickerDialog mTimePicker;
-    
-    mTimePicker = new TimePickerDialog(Planning.this,
-      new TimePickerDialog.OnTimeSetListener() {
-       @Override
-       public void onTimeSet(TimePicker timePicker,
-         int selectedHour, int selectedMinute) {
-        ViewHolder.datePickerButton.setText(selectedHour + ":"
-          + selectedMinute);
-       }
-      }, hour, minute, true);
-    mTimePicker.setTitle("Select Time");
-    mTimePicker.show();
-    updateDisplay();
-    
-    ViewHolder.time = hour+ ":" +minute+"";
-    Log.d("time", String.valueOf((ViewHolder.time)));
-    
-   } 
-   
-  });
-  loadSavedPreferences();
-  updateDisplay();
- 
- }
- 
+		});
+		loadSavedPreferences();
+		updateDisplay();
 
-	    
-	    public void updateDisplay( ) {
-	    	
-			ViewHolder.datePickerButton.setText
-	           // new StringBuilder().append("barev"));
-					(new StringBuilder().append(String.valueOf((phour))).append(":")
-		                     .append(String.valueOf((pminute))));
-					
-	 }
+	}
 
-	    
+	public void updateDisplay() {
 
+		ViewHolder.datePickerButton.setText
+		
+				(new StringBuilder().append(String.valueOf((phour)))
+						.append(":").append(String.valueOf((pminute))));
 
- private void loadSavedPreferences() {
-  // TODO Auto-generated method stub
-  sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+	}
 
-  
-  
-  boolean monState = sharedPreferences.getBoolean("MON",false);
-  ViewHolder.mon.setChecked(monState);
-  
-  boolean tueState = sharedPreferences.getBoolean("TUE",false);
-  ViewHolder.tue.setChecked(tueState);
-  
-  boolean wedState = sharedPreferences.getBoolean("WED",false);
-  ViewHolder.wed.setChecked(wedState);
-  
-  boolean thuState = sharedPreferences.getBoolean("THU",false);
-  ViewHolder.thu.setChecked(thuState);
-  
-  boolean friState = sharedPreferences.getBoolean("FRI",false);
-  ViewHolder.fri.setChecked(friState);
-  
-  boolean satState = sharedPreferences.getBoolean("SUT",false);
-  ViewHolder.sat.setChecked(satState);
-  
-  boolean sunState = sharedPreferences.getBoolean("SUN",false);
-  ViewHolder.sun.setChecked(sunState);
-  
-  
-  int name = sharedPreferences.getInt("counter", count);
+	private void loadSavedPreferences() {
+		
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-  String savedTime =  sharedPreferences.getString("TIME", ViewHolder.time);
-   ViewHolder.datePickerButton.setText(savedTime);
-  
-  countofdays.setText(name + " " + "DAY(S) SELECTED");
+		boolean monState = sharedPreferences.getBoolean("MON", false);
+		ViewHolder.mon.setChecked(monState);
 
- }
+		boolean tueState = sharedPreferences.getBoolean("TUE", false);
+		ViewHolder.tue.setChecked(tueState);
 
- private void savePreferences(String key, boolean value) {
-  sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-  Editor editor = sharedPreferences.edit();
+		boolean wedState = sharedPreferences.getBoolean("WED", false);
+		ViewHolder.wed.setChecked(wedState);
 
-  editor.putBoolean(key, value);
+		boolean thuState = sharedPreferences.getBoolean("THU", false);
+		ViewHolder.thu.setChecked(thuState);
 
-  editor.commit();
+		boolean friState = sharedPreferences.getBoolean("FRI", false);
+		ViewHolder.fri.setChecked(friState);
 
- }
+		boolean satState = sharedPreferences.getBoolean("SAT", false);
+		ViewHolder.sat.setChecked(satState);
 
- private void savePreferences(String key, int value) {
+		boolean sunState = sharedPreferences.getBoolean("SUN", false);
+		ViewHolder.sun.setChecked(sunState);
 
-  sharedPreferences = PreferenceManager
+		int name = sharedPreferences.getInt("counter", count);
 
-  .getDefaultSharedPreferences(this);
+		String savedTime = sharedPreferences.getString("TIME", ViewHolder.time);
+		ViewHolder.datePickerButton.setText(savedTime);
 
-  Editor editor = sharedPreferences.edit();
+		countofdays.setText(name + " " + "DAY(S) SELECTED");
 
-  editor.putInt(key, value);
+	}
 
-  editor.commit();
+	private void savePreferences(String key, boolean value) {
+		sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+		Editor editor = sharedPreferences.edit();
 
- }
- private void savePreferences(String key, String value) {
+		editor.putBoolean(key, value);
 
-  sharedPreferences = PreferenceManager
+		editor.commit();
 
-  .getDefaultSharedPreferences(this);
+	}
 
-  Editor editor = sharedPreferences.edit();
+	private void savePreferences(String key, int value) {
 
-  editor.putString(key, value);
+		sharedPreferences = PreferenceManager
 
-  editor.commit();
+		.getDefaultSharedPreferences(this);
 
- }
+		Editor editor = sharedPreferences.edit();
 
- OnCheckedChangeListener toggleButtonListener = new OnCheckedChangeListener() {
+		editor.putInt(key, value);
 
-  @Override
-  public void onCheckedChanged(CompoundButton buttonView,
-    boolean isChecked) {
-   if (!wasEdited) {
-    wasEdited = true;
-   }
-   if (buttonView.isChecked()) {
-    count++;
-    
-   } else
-    count--;
-   countofdays.setText(count + " " + "DAY(S) SELECTED");
-  
-   Log.d(String.valueOf((pminute)), String.valueOf(count));
+		editor.commit();
 
-  }
+	}
 
- };
+	private void savePreferences(String key, String value) {
 
- class TimePickerFragment extends DialogFragment implements
-   TimePickerDialog.OnTimeSetListener {
+		sharedPreferences = PreferenceManager
 
-  @Override
-  public Dialog onCreateDialog(Bundle savedInstanceState) {
-   // Use the current time as the default values for the picker
-   final Calendar c = Calendar.getInstance();
-   int hour = c.get(Calendar.HOUR_OF_DAY);
-   int minute = c.get(Calendar.MINUTE);
+		.getDefaultSharedPreferences(this);
 
-   // Create a new instance of TimePickerDialog and return it
-   return new TimePickerDialog(getActivity(), this, hour, minute,
-     DateFormat.is24HourFormat(getActivity()));
-   
-  }
+		Editor editor = sharedPreferences.edit();
 
-  public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-   // Do something with the time chosen by the user
-  }
+		editor.putString(key, value);
 
- }
+		editor.commit();
 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- protected void onPause() {
-  super.onPause();
-  if (wasEdited) {
-   savePreferences("counter", count);
-   savePreferences("MON", ViewHolder.mon.isChecked());
-   savePreferences("TUE", ViewHolder.tue.isChecked());
-   savePreferences("WED", ViewHolder.wed.isChecked());
-   savePreferences("THU", ViewHolder.thu.isChecked());
-   savePreferences("FRI", ViewHolder.fri.isChecked());
-   savePreferences("SAT", ViewHolder.sat.isChecked());
-   savePreferences("SUN", ViewHolder.sun.isChecked());
-   savePreferences("TIME", ViewHolder.time);
-   
-  }
+	}
 
- }
+	OnCheckedChangeListener toggleButtonListener = new OnCheckedChangeListener() {
 
- public static class ViewHolder {
-  static String time;
-  static ToggleButton mon;
-  static ToggleButton tue ;
-  static ToggleButton wed;
-  static ToggleButton thu;
-  static ToggleButton fri;
-  static ToggleButton sat;
-  static ToggleButton sun ;
-  static Button datePickerButton;
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView,
+				boolean isChecked) {
+			if (!wasEdited) {
+				wasEdited = true;
+			}
+			if (buttonView.isChecked()) {
+				count++;
 
- }
+			} else
+				count--;
+			countofdays.setText(count + " " + "DAY(S) SELECTED");
+			if (ViewHolder.mon.isChecked())
+				days = "mon";
+			// Log.d(time, "mon");
+			if (ViewHolder.tue.isChecked())
+				days += " " + " tue";
+			if (ViewHolder.wed.isChecked())
+				// Log.d(time, "wed");
+				days += " " + "wed";
+			if (ViewHolder.thu.isChecked())
+				// Log.d(time, "thu");
+				days += " " + "thu";
+			if (ViewHolder.fri.isChecked())
+				// Log.d(time,"fri");
+				days += " " + "fri";
+			if (ViewHolder.sat.isChecked())
+				// Log.d(time, "sat");
+				days += " " + "sat";
+			if (ViewHolder.sun.isChecked())
+				// Log.d(time, "sun");
+				days += " " + "sun";
+
+			Log.d(time, days);
+
+		}
+
+	};
+
+	class TimePickerFragment extends DialogFragment implements
+			TimePickerDialog.OnTimeSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			int minute = c.get(Calendar.MINUTE);
+
+			// Create a new instance of TimePickerDialog and return it
+			return new TimePickerDialog(getActivity(), this, hour, minute,
+					DateFormat.is24HourFormat(getActivity()));
+
+		}
+
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			// Do something with the time chosen by the user
+		}
+
+	}
+
+	protected void onPause() {
+		super.onPause();
+		if (wasEdited) {
+			savePreferences("counter", count);
+			savePreferences("MON", ViewHolder.mon.isChecked());
+			savePreferences("TUE", ViewHolder.tue.isChecked());
+			savePreferences("WED", ViewHolder.wed.isChecked());
+			savePreferences("THU", ViewHolder.thu.isChecked());
+			savePreferences("FRI", ViewHolder.fri.isChecked());
+			savePreferences("SAT", ViewHolder.sat.isChecked());
+			savePreferences("SUN", ViewHolder.sun.isChecked());
+			savePreferences("TIME", ViewHolder.time);
+
+		}
+
+	}
+
+	public static class ViewHolder {
+		static String time;
+		static ToggleButton mon;
+		static ToggleButton tue;
+		static ToggleButton wed;
+		static ToggleButton thu;
+		static ToggleButton fri;
+		static ToggleButton sat;
+		static ToggleButton sun;
+		static Button datePickerButton;
+
+	}
 }
